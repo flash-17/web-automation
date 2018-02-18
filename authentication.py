c748687bf804aed1371d9b3f2210e3c0e276e176
Ujjwal <ujjwalkhurana7@gmail.com>
2018-02-18 13:04:51 +0530

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -5,9 +5,9 @@ def login():
 	driver = webdriver.Chrome()
 	driver.get('http://172.16.2.1:1000/login?')
 	uid = driver.find_element_by_xpath('//*[@id="ft_un"]')
-	uid.send_keys('16BCS7017')
+	uid.send_keys('Your UID')
 	pw = driver.find_element_by_xpath('//*[@id="ft_pd"]')
-	pw.send_keys('AmaZ^1010')
+	pw.send_keys('Your PASSWORD')
 	pw.submit()
 
 def logout():
