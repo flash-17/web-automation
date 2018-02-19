d825a9fb56ae93ae3dda0cc0ba83830f7766cde9
Ujjwal <ujjwalkhurana7@gmail.com>
2018-02-20 04:24:44 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -5,25 +5,21 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
  
+def main():
+	driver = webdriver.Chrome()
+	driver.get("https://web.whatsapp.com/")
+	wait = WebDriverWait(driver, 10)
 
-driver = webdriver.Chrome()
- 
-driver.get("https://web.whatsapp.com/")
-wait = WebDriverWait(driver, 10)
- 
+	target = '"Reciever_name"'
+	string = "Cloud Ken Workshop"
+	x_arg = '//span[contains(@title,' + target + ')]'
+	group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+	group_title.click()
+	inp_xpath = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
+	input_box = wait.until(EC.presence_of_element_located((By.XPATH, inp_xpath)))
+	for i in range(100):
+		input_box.send_keys(string + Keys.ENTER)
+		time.sleep(1)
 
-target = '"Papa"'
- 
-
-string = "Message sent using Python!!!"
- 
-x_arg = '//span[contains(@title,' + target + ')]'
-group_title = wait.until(EC.presence_of_element_located((
-    By.XPATH, x_arg)))
-group_title.click()
-inp_xpath = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
-input_box = wait.until(EC.presence_of_element_located((
-    By.XPATH, inp_xpath)))
-for i in range(100):
-    input_box.send_keys(string + Keys.ENTER)
-    time.sleep(1)
+if __name__ == '__main__':
+	main()
