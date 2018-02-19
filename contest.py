15f8a6076a15beb623b5d1918c70aa475332c069
Ujjwal <ujjwalkhurana7@gmail.com>
2018-02-20 04:19:59 +0530

    Update contest.py

--- contest.py
+++ contest.py
@@ -1,16 +1,16 @@
 from selenium import webdriver
 import time
-def main():
 
+def main():
 	driver = webdriver.Chrome()
-	driver.get('https://cloudkentest.herokuapp.com')
-	username = driver.find_element_by_id('userid')
+	driver.get('http://cloudkentest.herokuapp.com')
+	username = driver.find_element_by_xpath('//*[@id="cloud"]')
 	username.send_keys('admin')
-	password = driver.find_element_by_id('password')
+	password = driver.find_element_by_xpath('//*[@id="ken"]')
 	password.send_keys('password')
+	time.sleep(4)
 	password.submit()
 	time.sleep(10)
-	
 
 if __name__ == '__main__':
 	main()
